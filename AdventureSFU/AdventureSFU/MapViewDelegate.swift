9e12e42f0e8b6c67c7c446c63b98b0cc7d0a0e31
Chris Norris-Jones <cnorrisj@asb9700-e02.csil.sfu.ca>
2017-03-06 06:50:49 -0800

    trying to continue merging

---
+++ AdventureSFU/AdventureSFU/MapViewDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewDelegate.swift
+//  AdventureSFU
+//
+//  Created by ela50 on 3/6/17.
+//  Copyright © 2017 Karan Aujla. All rights reserved.
+//
+import UIKit
+import Mapbox
+import MapboxDirections
+protocol MapViewDelegate {
+	
+	func getTime(time: Double) -> Double?
+	func getDistance(distance: Double) -> Double?
+	func getWaypoint(waypoint: Waypoint)
+	func getRoute(chosenRoute: Route) -> Route?
+	
+	
+	
+	/*
+	// MARK: - Navigation
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+	// Get the new view controller using segue.destinationViewController.
+	// Pass the selected object to the new view controller.
+	}
+	*/
+	
+}
--- AdventureSFU/AdventureSFU/ViewRunController.swift
+++ AdventureSFU/AdventureSFU/ViewRunController.swift
@@ -5,36 +5,77 @@
 //  Created by Carlos Abaffy paz on 3/4/17.
 //  Copyright © 2017 Karan Aujla. All rights reserved.
 //
-
 import UIKit
+import Mapbox
+import MapboxDirections
 
-class ViewRunController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+class ViewRunController: UIViewController, MapViewDelegate {
+	@IBOutlet weak var distanceField: UILabel!
+	@IBOutlet weak var timeField: UILabel!
+	var time: Double = 0
+	var distance: Double = 0
+	var waypoints: [Waypoint] = []
+	var route: Route?
 	
-	@IBAction func toRunBuddy() {
-		performSegue(withIdentifier: "StartRun", sender: self)
+	func getTime(time: Double) -> Double? {
+		self.time = time
+		timeField.text = String("min: \(time)")
+		print("searchabletime\(time)")
+		return time
 	}
-
+	
+	
+	func getDistance(distance: Double) -> Double? {
+		self.distance = distance
+		distanceField.text = String("kms: \(distance)")
+		print("searchabledistance\(time)")
+		return distance
+		
+	}
+	
+	func getWaypoint(waypoint: Waypoint) {
+		self.waypoints.append(waypoint)
+		
+	}
+	
+	func getRoute(chosenRoute: Route) -> Route? {
+		self.route = chosenRoute
+		print("searchableroute\(chosenRoute)")
+		return chosenRoute
+		
+	}
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
 	@IBAction func runToMain() {
 		performSegue(withIdentifier: "runControllerToMain", sender: self)
 	}
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	
+	// MARK: - Navigation
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		
+		if segue.identifier == "runpageembed" {
+			let childViewController = segue.destination as? MapUI
+			childViewController?.delegate = self
+			
+		}
+		
+		// Get the new view controller using segue.destinationViewController.
+		// Pass the selected object to the new view controller.
+	}
+ 
+	
 }
